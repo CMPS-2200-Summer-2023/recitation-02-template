9e546a367fdf95cbd6091245b7f055b1ac58bc3a
arminia <6212583+arminia@users.noreply.github.com>
2020-08-30 21:46:26 -0500

    edited the main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def simple_work_calc(n, a, b):
 
 def test_simple_work():
 	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == #TODO
+	assert simple_work_calc(20, 3, 2) == #TODO
+	assert simple_work_calc(30, 4, 2) == #TODO
 
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -62,7 +62,7 @@ def test_work():
 	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
 	assert work_calc(30, 3, 2, lambda n: n) == #TODO
 
-def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+def compare_work(sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
 	Compare the values of different recurrences for 
 	given input sizes.
@@ -82,22 +82,54 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 			))
 	return result
 
-def print_results(results):
+def print_work_results(results):
 	""" done """
 	print(tabulate.tabulate(results,
 							headers=['n', 'W_1', 'W_2'],
 							floatfmt=".3f",
 							tablefmt="github"))
 
+def print_span_results(results):
+	""" done """
+	print(tabulate.tabulate(results,
+							headers=['n', 'S_1', 'S_2'],
+							floatfmt=".3f",
+							tablefmt="github"))
 def test_compare_work():
 	# curry work_calc to create multiple work
 	# functions taht can be passed to compare_work
     
 	# create work_fn1
-	# create work_fn2
+		# create work_fn2
+	
+	res = compare_work(work_fn1, work_fn2)
+	print_work_results(res)
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+def compare_span(sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+	"""
+	Compare the values of different recurrences for 
+	given input sizes.
+
+	Returns:
+	A list of tuples of the form
+	(n, work_fn1(n), work_fn2(n), ...)
+	
+	"""
+	result = []
+	for n in sizes:
+		# compute W(n) using current a, b, f
+		result.append((
+			n,
+			span_fn1,
+			span_fn2
+			))
+	return result
 
 def test_compare_span():
-	# TODO
+	assert span_calc(10, 2, 2,lambda n: 1) == #TODO
+	assert span_calc(20, 1, 4, lambda n: n*n) == #TODO
+	assert span_calc(30, 3, 4, lambda n: n) == #TODO
+
+	res = compare_span()
+	print_span_results(res)
+	pass
